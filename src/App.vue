ac5fbb8aae0cb93d3a1f40c77ae8850b3a8ecdd4
umaporn-sup <umaporn@sit.kmutt.ac.th>
2023-03-01 22:23:06 +0700

    [add] git repo

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+import { getMails } from './composable/getMails.js'
+import SingleMail from './components/SingleMail.vue'
+const mails = ref(getMails())
+// console.log(mails)
+</script>
+<template>
+  <div class="w-full p-3">
+    <div>
+      <h1 class="flex justify-center text-2xl font-semibold">
+        ~ Mailing List ~
+      </h1>
+    </div>
+    <div class="w-full flex">
+      <div class="flex w-2/12 justify-center tracking-widest font-semibold">
+        Title
+      </div>
+      <div
+        class="flex w-8/12 justify-self-center tracking-widest font-semibold"
+      >
+        Body
+      </div>
+      <div class="flex w-2/12 justify-center tracking-widest font-semibold">
+        Keywords
+      </div>
+    </div>
+    <div class="w-full flex" v-for="mail in mails" :key="mail.id">
+      <SingleMail :mail="mail" />
+    </div>
+    <!-- <div class="grid grid-cols-8 gap-2">
+      <div class="justify-self-center tracking-widest font-semibold">Title</div>
+      <div class="col-span-6 justify-self-center tracking-widest font-semibold">
+        Body
+      </div>
+      <div class="justify-self-center tracking-widest font-semibold">
+        Keywords
+      </div>
+    </div>
+    <div class="grid grid-cols-8 gap-2" v-for="mail in mails" :key="mail.id">
+      <SingleMail
+        :mail="mail"
+        :layout="{ type: 'grid', colSpanNo: 2, colSpanValue: 6 }"
+      />
+    </div> -->
+  </div>
+</template>
+<style scoped></style>
---
+++ src/components/SingleMail.vue
@@ -0,0 +1,50 @@
+<script setup>
+const props = defineProps({
+  mail: {
+    type: Object,
+    required: true
+  },
+  layout: {
+    type: Object,
+    default: {
+      type: 'flex'
+    }
+  }
+})
+</script>
+<template>
+  <div
+    :class="
+      layout.type === 'flex'
+        ? 'w-2/12'
+        : layout.colSpanNo === 1
+        ? `col-span-${layout.colSpanValue}`
+        : ''
+    "
+  >
+    <input type="checkbox" value="mail.id" /> <span>{{ mail.title }}</span>
+  </div>
+  <div
+    :class="
+      layout.type === 'flex'
+        ? 'w-8/12'
+        : layout.colSpanNo === 2
+        ? `col-span-${layout.colSpanValue}`
+        : ''
+    "
+  >
+    {{ mail.body }}
+  </div>
+  <div
+    :class="
+      layout.type === 'flex'
+        ? 'w-2/12'
+        : layout.colSpanNo === 3
+        ? `col-span-${layout.colSpanValue}`
+        : ''
+    "
+  >
+    {{ mail.keywords.join('/ ') }}
+  </div>
+</template>
+<style scoped></style>
